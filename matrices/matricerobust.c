5d1f2a3475ec361b24c2584c286555993ae455ab
Raja Sumeer <rajamsumeer@gmail.com>
2025-10-09 02:10:02 +0100

    matrices

--- matrices/matricerobust.c
+++ matrices/matricerobust.c
@@ -8,37 +8,32 @@ Date: 03/12/2024 16:10
 Code Purpose:
 Matrice manipulation and arithmetic of matrices
 Using functions for redundancy
-
-Tasks:
-
-Supporting Code from Author(s):
-Due Credits: 
 */
 
-#include <stdio.h> // define preprocessor directive library for basic functions
+#include <stdio.h>
 
 // function to create Array1.txt
 void writeFile1() {
-FILE *oneMatrix; // define file pointers
+FILE *oneMatrix;
 oneMatrix = fopen("Array1.txt", "w"); // create the initial Array1 txt file
-fprintf(stdout, "Array1.txt has been created.\n"); // print out to let you know first file was created
-fclose(oneMatrix); // close file
+fprintf(stdout, "Array1.txt has been created.\n");
+fclose(oneMatrix);
 }
 
 // function to create Array2.txt
 void writeFile2() {
-FILE *secondMatrix; // define file pointers
+FILE *secondMatrix;
 secondMatrix = fopen("Array2.txt", "w"); // create the initial Array2 txt file
-fprintf(stdout, "Array2.txt has been created.\n"); // print out to let you know second file was created
-fclose(secondMatrix); // close file
+fprintf(stdout, "Array2.txt has been created.\n");
+fclose(secondMatrix);
 }
 
 // function to create Array3.txt
 void writeFile3() {
-FILE *thirdMatrix; // define file pointers
+FILE *thirdMatrix;
 thirdMatrix = fopen("Array3.txt", "w"); // create the initial Array3 txt file
-fprintf(stdout, "Array3.txt has been created.\n"); // print out to let you know third file was created
-fclose(thirdMatrix); // close file
+fprintf(stdout, "Array3.txt has been created.\n");
+fclose(thirdMatrix);
 }
 
 // function to write values to Matrix A (Array1.txt)
@@ -46,22 +41,22 @@ int writeMatrixA() {
 int counter = 0; // int counter to count matrix R x C
 int matrixA[3][2];
 int row, col;
-FILE *oneMatrix; // define file pointers
-oneMatrix = fopen("Array1.txt", "w"); // open the created Array1 file
+FILE *oneMatrix;
+oneMatrix = fopen("Array1.txt", "w");
 if (oneMatrix == NULL) { // check reading state => file exists or can be accessed
     fprintf(stdout, "Array1.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 3; // return error code 3 to shell, operation unsuccessful
+    return 1;
 }
-for (row = 0; row < 3; row++) { // increment through rows
-    for (col = 0; col < 2; col++) { // increment through columns
-        matrixA[row][col] = counter++; // increment count row + column array 
-        fprintf(oneMatrix, "%d\t", matrixA[row][col]); // write to Array1.txt
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 2; col++) {
+        matrixA[row][col] = counter++;
+        fprintf(oneMatrix, "%d\t", matrixA[row][col]);
     }
-    fprintf(oneMatrix, "\n"); // new line to seperate
+    fprintf(oneMatrix, "\n");
 } // end of for loop
-fclose(oneMatrix); // close the file with changes after all array values have been written to Array1.txt
-fprintf(stdout, "Array1.txt successful.\n"); // print out process result
-return 0; // return success code to shell
+fclose(oneMatrix);
+fprintf(stdout, "Array1.txt successful.\n");
+return 0;
 }
 
 // function to write values to Matrix B (Array2.txt)
@@ -69,77 +64,77 @@ int writeMatrixB() {
 int secondcounter = 0; // int counter to count matrix R x C
 int matrixB[2][3];
 int row, col;
-FILE *secondMatrix; // define file pointers
-secondMatrix = fopen("Array2.txt", "w"); // open the created Array2 file
+FILE *secondMatrix;
+secondMatrix = fopen("Array2.txt", "w");
 if (secondMatrix == NULL) { // check reading state => file exists or can be accessed
     fprintf(stdout, "Array2.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 3; // return error code 3 to shell, operation unsuccessful
+    return 1;
 }
 // initialize matrix B with values and write contents to Array2.txt
-for (row = 0; row < 2; row++) { // increment through rows
-    for (col = 0; col < 3; col++) { // increment through columns
-        matrixB[row][col] = secondcounter++; // increment count row + column array 
-        fprintf(secondMatrix, "%d\t", matrixB[row][col]); // write to Array2.txt
+for (row = 0; row < 2; row++) {
+    for (col = 0; col < 3; col++) {
+        matrixB[row][col] = secondcounter++;
+        fprintf(secondMatrix, "%d\t", matrixB[row][col]);
     }
-    fprintf(secondMatrix, "\n"); // new line to seperate
+    fprintf(secondMatrix, "\n");
 } // end of for loop
-fclose(secondMatrix); // close the file with changes after all array values have been written to Array2.txt
-fprintf(stdout, "Array2.txt successful.\n"); // print out process result
-return 0; // return success code to shell
+fclose(secondMatrix);
+fprintf(stdout, "Array2.txt successful.\n");
+return 0;
 }
 
 // function to read in values from Array1.txt and populate matrix A
 int readMatrixA(int matrixA[3][2]) {
-int row, col; // define variables for row, column
-FILE *readArrayFile; // define file pointers
-readArrayFile = fopen("Array1.txt", "r"); // read Array1.txt file
+int row, col;
+FILE *readArrayFile;
+readArrayFile = fopen("Array1.txt", "r");
 if (readArrayFile == NULL) { // check reading state => file exists or can be accessed
     fprintf(stdout, "Array1.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 3; // return error code 3 to shell, operation unsuccessful
+    return 1;
 }
 
 // reading in values from Array1.txt to matrix A 
-for (row = 0; row < 3; row++) { // read rows
-    for (col = 0; col < 2; col++) { // read columns
-    fscanf(readArrayFile, "%d", &matrixA[row][col]); // read in values to matrix A
-    // fprintf(stdout, "%d\t", matrixA[row][col]); // print out matrix A
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 2; col++) {
+    fscanf(readArrayFile, "%d", &matrixA[row][col]);
+    // fprintf(stdout, "%d\t", matrixA[row][col]);
     }
-    // fprintf(stdout, "\n"); // new line to seperate
+    // fprintf(stdout, "\n");
 } // end of for loop
-fclose(readArrayFile); // close Array1.txt after all values read
-return 0; // return success code to shell
+fclose(readArrayFile);
+return 0;
 }
 
 // function to read in values from Array2.txt and populate matrix B
 int readMatrixB(int matrixB[2][3]) {
-int row, col; // define variables for row, column
-FILE *readArrayFile2; // define file pointers
-readArrayFile2 = fopen("Array2.txt", "r"); // read Array2.txt file
+int row, col;
+FILE *readArrayFile2;
+readArrayFile2 = fopen("Array2.txt", "r");
 if (readArrayFile2 == NULL) { // check reading state => file exists or can be accessed
     printf("Array2.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 2; // return error code 1 to shell, operation unsuccessful
+    return 1;
 }
 
 // reading in values from Array2.txt to matrix B
-for (row = 0; row < 2; row++) { // read rows
-    for (col = 0; col < 3; col++) { // read columns
-    fscanf(readArrayFile2, "%d", &matrixB[row][col]); // read in values to matrix B
-    // fprintf(stdout, "%d\t", matrixB[row][col]); // print out matrix B
+for (row = 0; row < 2; row++) {
+    for (col = 0; col < 3; col++) {
+    fscanf(readArrayFile2, "%d", &matrixB[row][col]);
+    // fprintf(stdout, "%d\t", matrixB[row][col]);
     }
-    // fprintf(stdout, "\n"); // new line to seperate
+    // fprintf(stdout, "\n");
 } // end of for loop
-fclose(readArrayFile2); // close Array2.txt after all values read
-return 0; // return success code to shell
+fclose(readArrayFile2);
+return 0;
 }
 
 // function to multiply matrix A and B and store in matrix C
 void matrixMultiplyC(int matrixC[3][3], int matrixA[3][2], int matrixB[2][3]) {
-int row, col, k; // define variables for row, column and extra variable
-for (row = 0; row < 3; row++) { // increment rows
-    for (col = 0; col < 3; col++) { // increment columns
+int row, col, k;
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
         matrixC[row][col] = 0; // initialize matrixC to 0 to avoid junk values
          for (k = 0 ; k < 2; k++) { // columns
-        matrixC[row][col] += matrixA[row][k] * matrixB[k][col]; // multiply a and b to get result and store in matrix c
+        matrixC[row][col] += matrixA[row][k] * matrixB[k][col];
          }
     }
 } // end of for loop
@@ -147,113 +142,113 @@ for (row = 0; row < 3; row++) { // increment rows
 
 // function to send the result of matrix C to Command Line
 void matrixMultiplyCResult(int matrixC[3][3]) {
-int row, col; // define variables for row, column
-fprintf(stdout, "\nResult of Matrix A x Matrix B:\n"); // print out to notify about result
+int row, col;
+fprintf(stdout, "\nResult of Matrix A x Matrix B:\n");
 // print out multiplication result of matrix a and b from matrix c
-for (row = 0; row < 3; row++) { // increment rows
-    for (col = 0; col < 3; col++) { // increment columns
-        fprintf(stdout, "%d\t", matrixC[row][col]); // print out matrix c
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
+        fprintf(stdout, "%d\t", matrixC[row][col]);
     }
-    fprintf(stdout, "\n"); // new line to seperate
+    fprintf(stdout, "\n");
 } // end of for loop
 }
 
 int writeMatrixD(int matrixD[3][3]) {
 int thirdcounter = 0; // another counter for matrix d
-int row, col; // define variables for row, column
-FILE *thirdMatrix; // define file pointers
-thirdMatrix = fopen("Array3.txt", "w"); // open the created Array3 file
+int row, col;
+FILE *thirdMatrix;
+thirdMatrix = fopen("Array3.txt", "w");
 if (thirdMatrix == NULL) { // check reading state => file exists or can be accessed
     printf("Array3.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 3; // return error code 3 to shell, operation unsuccessful
+    return 1;
 }
 // initialize matrix D with values and write contents to Array3.txt
-for (row = 0; row < 3; row++) { // increment through rows
-    for (col = 0; col < 3; col++) { // increment through columns
-        matrixD[row][col] = thirdcounter++; // increment count row + column array 
-        fprintf(thirdMatrix, "%d\t", matrixD[row][col]); // write to Array3.txt
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
+        matrixD[row][col] = thirdcounter++; 
+        fprintf(thirdMatrix, "%d\t", matrixD[row][col]);
     }
-    fprintf(thirdMatrix, "\n"); // new line to seperate
+    fprintf(thirdMatrix, "\n");
 } // end of for loop
-fclose(thirdMatrix); // close the file with changes after all array values have been written to Array3.txt
-fprintf(stdout, "\nArray3.txt successful.\n"); // print out process result
-return 0; // return success code to shell
+fclose(thirdMatrix);
+fprintf(stdout, "\nArray3.txt successful.\n");
+return 0;
 }
 
 int readMatrixD(int matrixD[3][3]) {
-int row, col; // define variables for row, column
-FILE *readArrayFile3; // define file pointers
-readArrayFile3 = fopen("Array3.txt", "r"); // read Array3.txt file
+int row, col;
+FILE *readArrayFile3;
+readArrayFile3 = fopen("Array3.txt", "r");
 if (readArrayFile3 == NULL) { // check reading state => file exists or can be accessed
     printf("Array3.txt cannot be accessed for reading. Verify it exists and try again.\n");
-    return 3; // return error code 3 to shell, operation unsuccessful
+    return 1;
 }
 
 // reading in values from Array3.txt to matrix D
-for (row = 0; row < 3; row++) { // read rows
-    for (col = 0; col < 3; col++) { // read columns
-    fscanf(readArrayFile3, "%d", &matrixD[row][col]); // read in values to matrix D
-    // fprintf(stdout, "%d\t", matrixD[row][col]); // print out matrix D
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
+    fscanf(readArrayFile3, "%d", &matrixD[row][col]);
+    // fprintf(stdout, "%d\t", matrixD[row][col]);
     }
-    // fprintf(stdout, "\n"); // new line to seperate
+    // fprintf(stdout, "\n");
 } // end of for loop
-fclose(readArrayFile3); // close Array3.txt after all values read
-return 0; // return success code to shell
+fclose(readArrayFile3);
+return 0;
 }
 
 void matrixEAdd(int matrixE[3][3], int matrixC[3][3], int matrixD[3][3]) {
-int row, col; // define variables for row, column
-for (row = 0; row < 3; row++) { // increment rows
-    for (col = 0; col < 3; col++) { // increment columns
+int row, col;
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
         matrixE[row][col] = 0; // initialize matrixE to 0 to avoid junk values
-        matrixE[row][col] = matrixC[row][col] + matrixD[row][col]; // add c and d to get result and store in matrix E
+        matrixE[row][col] = matrixC[row][col] + matrixD[row][col];
     }
 } // end of for loop
 }
 
 void matrixEAddResult(int matrixE[3][3]) {
-int row, col; // define variables for row, column
-fprintf(stdout, "\nResult of Matrix C + Matrix D:\n"); // print out to notify about result
+int row, col;
+fprintf(stdout, "\nResult of Matrix C + Matrix D:\n");
 // print out addition result of matrix c and d from matrix e
-for (row = 0; row < 3; row++) { // increment rows
-    for (col = 0; col < 3; col++) { // increment columns
-        fprintf(stdout, "%d\t", matrixE[row][col]); // print out matrix c
+for (row = 0; row < 3; row++) {
+    for (col = 0; col < 3; col++) {
+        fprintf(stdout, "%d\t", matrixE[row][col]);
     }
-    fprintf(stdout, "\n"); // new line to seperate
+    fprintf(stdout, "\n");
 } // end of for loop
 }
 
 int main() { // start of & definition of main loop
 
-writeFile1(); // call function
-writeFile2(); // call function
-writeFile3(); // call function
+writeFile1();
+writeFile2();
+writeFile3();
 
 int matrixA[3][2];
 int matrixB[2][3];
 int matrixC[3][3];
 
-writeMatrixA(); // call function
-writeMatrixB(); // call function
+writeMatrixA();
+writeMatrixB();
 
 // Part (i)
-readMatrixA(matrixA); // call function
-readMatrixB(matrixB); // call function
+readMatrixA(matrixA);
+readMatrixB(matrixB);
 
 // Part (ii) A x B
-matrixMultiplyC(matrixC, matrixA, matrixB); // call function
-matrixMultiplyCResult(matrixC); // call function
+matrixMultiplyC(matrixC, matrixA, matrixB);
+matrixMultiplyCResult(matrixC);
 
 // Part (iii)
 
 int matrixD[3][3];
 int matrixE[3][3];
 
-writeMatrixD(matrixD); // call function
-readMatrixD(matrixD); // call function
+writeMatrixD(matrixD);
+readMatrixD(matrixD);
 
-matrixEAdd(matrixE, matrixC, matrixD); // call function
-matrixEAddResult(matrixE); // call function
+matrixEAdd(matrixE, matrixC, matrixD);
+matrixEAddResult(matrixE);
 
 return 0;
 
